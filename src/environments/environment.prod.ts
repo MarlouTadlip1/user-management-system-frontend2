c7376c2b1a30550940fb4b93badf9467bd3fc916
Marlo Gallego <148179990+Mawlow@users.noreply.github.com>
2025-05-18 20:35:43 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'http://localhost:4000'
+  apiUrl: process.env['API_URL'] || 'https://user-management-system-backend-lq0h.onrender.com'
 };
