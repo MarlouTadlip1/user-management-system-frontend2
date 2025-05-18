823c09670663c639a76e41db3b281e99bd54c75b
aechan444 <136810093+aechan444@users.noreply.github.com>
2025-05-18 22:52:42 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl:
     process.env['API_URL'] ||
-    'https://user-management-system-backend-0ynv.onrender.com',
+    'https://user-management-system-backend-er6u.onrender.com',
 };
