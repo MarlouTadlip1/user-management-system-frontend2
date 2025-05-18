136b489f2ab5065c6c1ef95ec6a014d267afe125
JonaidAsal <jonaidasal3@gmail.com>
2025-05-18 20:53:06 +0800

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'http://localhost:4000'
+  apiUrl: process.env['API_URL'] || 'https://user-management-system-backend-0ajg.onrender.com'
 };
